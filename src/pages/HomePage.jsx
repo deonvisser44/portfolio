7ece92d75aefbe2fdd5caf1bd1111c76602e9a74
deonvisser44 <deonvisser44@gmail.com>
2023-07-20 22:01:50 +0700

    change styling

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
-import React from "react";
-import classes from "./HomePage.module.css";
-import emoji from '../assets/emoji.png'
+import React from 'react';
+import classes from './HomePage.module.css';
+import emoji from '../assets/emoji.png';
 import { HiOutlineDocumentDownload } from 'react-icons/hi';
 import resume from '../assets/DeonVisserCV2022.pdf';
 
@@ -8,23 +8,31 @@ function HomePage() {
   return (
     <div className={classes.homePage}>
       <div className={classes.introWrapper}>
-      <div className={classes.introText}>
-        <div className={classes.textWrapper}>
-          <div className={classes.textWrapperHead}>
-            <h1>Web Developer</h1>
-          </div>
-          <p>
-            Hi, I'm Deon, I have a passion for helping people bring their technical ideas to life.
-          </p>
-          <div className={classes.buttonWrapper}>
-            <a href={resume} download="DeonVisserCV.pdf" className={classes.downloadButton}>Get Resume<HiOutlineDocumentDownload /></a>
+        <div className={classes.introText}>
+          <div className={classes.textWrapper}>
+            <div className={classes.textWrapperHead}>
+              <h1>Web Developer</h1>
+            </div>
+            <p>
+              Hi, I'm Deon, I have a passion for helping people bring their
+              technical ideas to life.
+            </p>
+            <div className={classes.buttonWrapper}>
+              <a
+                href={resume}
+                download='DeonVisserCV.pdf'
+                className={classes.downloadButton}
+              >
+                Get Resume
+                <HiOutlineDocumentDownload />
+              </a>
+            </div>
           </div>
         </div>
+        <div className={classes.imageWrapper}>
+          <img src={emoji} alt='me' />
+        </div>
       </div>
-      <div className={classes.imageWrapper}>
-        <img src={emoji} alt="me" />
-      </div>
-    </div>
     </div>
   );
 }
